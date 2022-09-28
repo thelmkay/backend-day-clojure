ee08457125773db40f4765fa05d84e4e391c06ab
xdesign-thomashelmkay <thomas.helmkay@xdesign.com>
2022-09-28 12:15:33 +0100

    added solution

--- src/backend_day/core.clj
+++ src/backend_day/core.clj
@@ -9,14 +9,84 @@ then we trim any trailing whitespace from each element via map command"
   (map str/trim (str/split (str/trim-newline (slurp "input.csv")) #",")))
 
 
+
+(def vector-map {:N [0 1] :E [1 0] :S [0 -1] :W [-1 0]})
+(defn next-heading [heading direction]
+  "Get the next heading based on the turn direction, headings are based on a simple
+compass North East South West which help define our movement direction. Heading should
+be the current heading and direction from the current direction step"
+  (cond
+    (and (= heading :N) (= direction "R")) :E
+    (and (= heading :N) (= direction "L")) :W
+    (and (= heading :E) (= direction "R")) :S
+    (and (= heading :E) (= direction "L")) :N
+    (and (= heading :S) (= direction "R")) :W
+    (and (= heading :S) (= direction "L")) :E
+    (and (= heading :W) (= direction "R")) :N
+    (and (= heading :W) (= direction "L")) :S))
+
+(defn gen-vector [heading distance]
+  "Generate a movement vector from our vector map which defines directions for
+N E S W"
+  (map (fn [x] (* x distance)) (heading vector-map)))
+
+
+
+(defn distance [direction] (Integer/parseInt (last (.split direction "R|L"))))
+
+(defn turn
+  "Extract the turn from the direction step which is an L or R"
+  [direction] (str (nth direction 0)))
+
+(defn translate [location move]
+  "Apply a movement vector to the location such that the x y of the location 
+is moved up or down on a cartesian coordinate grid according to the move variable
+ e.g [1 -1] moves it one down and one to the right "
+  (let [[x y] location [mx my] move]
+    (vector (+ x mx) (+ y my))))
+
+(defn final-distance [sx sy location]
+  "Compute the taxi cab distance between two points 0 0 [x y]"
+  (let [[x y] location] (+ (abs (- sx x)) (abs (- sy y)))))
+
+(defn gen-visited [location destination heading grid-map]
+  "Generate a map of visited blocks in this transformations such that each block vistited
+is represented in a map by its coord and a value for the times visited {[0 1] 1} we need to
+walk through each block one by one and record a visit"
+  (let [[sx sy] location distance-t (final-distance sx sy destination)]
+    ;; recursive loop
+    (loop [steps (range 0 distance-t) current-location location visited grid-map]
+      (let [block-location (translate current-location (heading vector-map))]
+        (cond (= (count steps) 0) visited
+              :else (recur (rest steps) block-location (assoc visited block-location (inc (get visited block-location 0)))))))))
+
+;; location is 0,0 track cordinate changes based on processing each direction
+;; such that each direction is a vector translation then when we have the final
+;; vector compute the distance between them 
+(defn solution-part-2 [directions heading location grid-map]
+  (cond
+    (> (count (filter (fn [[k v]] (> v 1)) grid-map)) 0) (final-distance 0 0 (first (keys (filter (fn [[k v]] (> v 1)) grid-map))))
+    (= (count directions) 0) (final-distance 0 0 location)
+
+    :else (let [head (first directions)
+                move-vec (gen-vector (next-heading heading (turn head)) (distance head))
+                updated-map (gen-visited location (translate location move-vec) (next-heading heading (turn head)) grid-map)]
+            (solution-part-2 (rest directions) (next-heading heading (turn head)) (translate location move-vec) updated-map))))
+
 (defn solution [directions heading location]
   "Directions [ 'R4','L3','R2']
    heading initial heading :N for north
    location, starting location x y[0 0]"
-  ())
+  (cond (= (count directions) 0) (final-distance 0 0 location)
+        :else (let [head (first directions)
+                    move-vec (gen-vector (next-heading heading (turn head)) (distance head))]
+                ;; tail call
+                (solution (rest directions) (next-heading heading (turn head)) (translate location move-vec)))))
+
 
 (defn -main
   "Main method"
   [& args]
 
-  (println (solution (read-input) :N [0 0])))
+  (println (solution (read-input) :N [0 0]))
+  (println (solution-part-2 (read-input) :N [0 0] {})))
--- test/backend_day/core_test.clj
+++ test/backend_day/core_test.clj
@@ -3,6 +3,31 @@
             [backend-day.core :refer :all]))
 
 
+
+(deftest translate-test
+  (testing "translate vector [2 , 3] by 2 left 3 up [-2, 3]"
+    (is (= [0, 6] (translate [2,3] [-2, 3])))))
+
+(deftest genVector-test
+  (testing "genvector test east movement vector")
+  (is (= [3 0] (gen-vector :E 3))))
+
+(deftest gen-visited-test
+  (testing "Generate a map of the path taken e.g {[0 1] 1 [0 2] 1}"
+    (is (= {[1 0] 1 [2 0] 1 [3 0] 1} (gen-visited [0 0] [3 0] :E {})))))
+
+(deftest gen-visited-test-1
+  (testing "Generate a map of the path taken going west"
+    (is (= {[-1 0] 1 [-2 0] 1 [-3 0] 1} (gen-visited [0 0] [-3 0] :W {})))))
+
+(deftest gen-visited-test-2
+  (testing "Generate a map of the path taken that intersects with an existing path"
+    (is (= {[1 0] 1 [2 0] 2 [3 0] 1 [2 1] 1 [2 2] 1 [2 3] 1} (gen-visited [2 -1] [2 3] :N {[1 0] 1 [2 0] 1 [3 0] 1})))))
+
+(deftest turn-test
+  (testing "turn should be R from R3"
+    (is (= "R" (turn "R3")))))
+
 (deftest test-solution-day-1-basic
   (testing "R2 L3 5 blocks away"
     (is (= 5 (solution ["R2","L3"] :N [0,0])))))
@@ -15,10 +40,10 @@
   (testing "R5 L5 R5 R3 12 blocks away"
     (is (= 12 (solution ["R5","L5","R5","R3"] :N [0,0])))))
 
-;;(deftest test-adevent-day-1-part-2
-;;  (testing "R8, R4, R4, R8 first location visit twice 4 blocks away"
-;;    (is (= 4 (solution ["R8","R4","R4","R8"] :N [0,0] {})))))
+(deftest test-adevent-day-1-part-2
+  (testing "R8, R4, R4, R8 first location visit twice 4 blocks away"
+    (is (= 4 (solution-part-2 ["R8","R4","R4","R8"] :N [0,0] {})))))
 
-;;(deftest test-advent-day-1-part-2
-;;  (testing "R4 L4 L2 L2 L5 first visit 5 block away"
-;;    (is (= 6 (solution ["R4", "L4","L2","L2","L5"] :N [0,0] {})))))
+(deftest test-advent-day-1-part-2
+  (testing "R4 L4 L2 L2 L5 first visit 5 block away"
+    (is (= 6 (solution-part-2 ["R4", "L4","L2","L2","L5"] :N [0,0] {})))))
