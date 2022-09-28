863b57c6d71e48acc17167d6d81d50b36e22798e
xdesign-thomashelmkay <thomas.helmkay@xdesign.com>
2022-09-28 12:16:30 +0100

    rename

--- test/backend_day/core_test.clj
+++ test/backend_day/core_test.clj
@@ -15,10 +15,10 @@
   (testing "R5 L5 R5 R3 12 blocks away"
     (is (= 12 (solution ["R5","L5","R5","R3"] :N [0,0])))))
 
-;;(deftest test-adevent-day-1-part-2
+;;(deftest test-solution-day-1-part-2
 ;;  (testing "R8, R4, R4, R8 first location visit twice 4 blocks away"
 ;;    (is (= 4 (solution ["R8","R4","R4","R8"] :N [0,0] {})))))
 
-;;(deftest test-advent-day-1-part-2
+;;(deftest test-solution-day-1-part-2
 ;;  (testing "R4 L4 L2 L2 L5 first visit 5 block away"
 ;;    (is (= 6 (solution ["R4", "L4","L2","L2","L5"] :N [0,0] {})))))
